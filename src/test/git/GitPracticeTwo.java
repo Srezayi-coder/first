8ef4a91be10bca5511d1bbe25b585700139768e3
rseddiqa <rseddiqa@gmail.com>
2023-02-27 14:18:09 -0500

    this is commit

---
+++ src/test/git/GitPracticeTwo.java
@@ -0,0 +1,16 @@
+package test.git;
+
+public class GitPracticeTwo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("result is " + multiply(2, 9));
+
+	}
+	
+	public static int multiply(int a, int b) {
+		int result = a * b;
+		return result;
+	}
+
+}
